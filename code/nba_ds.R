e58cf9ff3cc380979ebcd34bf6d4532efe6aa532
Lucas Bishop <bishoplucas95@gmail.com>
2020-07-29 08:19:51 -0400

    full dataset finalized, ready for analysis

--- code/nba_ds.R
+++ code/nba_ds.R
@@ -14,8 +14,8 @@ html_pages <- c(1:3) %>%
   paste("https://basketball.realgm.com/nba/stats/2019/Misc_Stats/Qualified/dbl_dbl/All/desc/",.)
 
 
-traditional <- read_csv("datafun/36minutes.csv") %>% select(-badcol)
-advanced <- read_csv("datafun/advancedNBA.csv") %>% select(-badcol)
+traditional <- read_csv("data/36minutes.csv") %>% select(-badcol)
+advanced <- read_csv("data/advancedNBA.csv") %>% select(-badcol)
 # make last df with our above functions
 #extra <- map_dfr(html_pages, pull_player_data)
   
@@ -27,7 +27,8 @@ extra2 <- read_html("https://basketball.realgm.com/nba/stats/2019/Misc_Stats/Qua
 extra3 <- read_html("https://basketball.realgm.com/nba/stats/2019/Misc_Stats/Qualified/dbl_dbl/All/desc/ 1") %>% 
   html_table("table#table-4392.tablesaw.compact.tablesaw-stack", header = NA, fill = TRUE)
 
-misc <- rbind(extra1[[11]], extra2[[11]], extra3[[11]]) %>% select(-'#')
+# get rid of the win share stats that are redundant in misc dataset
+misc <- rbind(extra1[[1]], extra2[[1]], extra3[[1]]) %>% select(-'#', -"OWS", -"DWS", -"WS")
 merged_stats <- inner_join(traditional, advanced) %>% select(-Rk) 
   
 ## cleanup before misc merge
@@ -36,7 +37,22 @@ misc$Player=gsub(",", "", misc$Player, fixed = TRUE)
 # Had initally tried to replace all of the special characters in foreign names, but eventually just changed the csv files before reading in
 
 #Now have full data frame with combined data - can add more columns if I find extra exportable stats websites
-merged_stats <- full_join(merged_stats, misc, by = 'Player') %>% unique()
+full_stats <- full_join(merged_stats, misc, by = 'Player') %>% unique()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
